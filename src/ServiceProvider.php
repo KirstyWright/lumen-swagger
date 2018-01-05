a4a05d23c2f293d0aa2da0add5014b0541830a8b
Benjamin Crozat <benjamin@croz.at>
2018-01-05 13:42:47 +0100

    Register command only if running in console

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,17 +5,12 @@ namespace BC\LumenSwagger;
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
      * Register package's services.
      */
     public function register()
     {
-        $this->commands(SwaggerCommand::class);
+        if ($this->app->runningInConsole()) {
+            $this->commands(SwaggerCommand::class);
+        }
     }
 }
