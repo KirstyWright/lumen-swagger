63e8524fead596b3ceb47c5b8e13e7cf4e1e6c4a
Benjamin Crozat <b.crozat@qwant.com>
2018-01-05 10:53:05 +0100

    Forgot to setup autoloading

--- src/SwaggerCommand.php
+++ src/SwaggerCommand.php
@@ -9,7 +9,7 @@ class SwaggerCommand extends \Illuminate\Console\Command
      *
      * @var string
      */
-    protected $signature = 'swagger';
+    protected $signature = 'swagger {--p|path=}';
 
     /**
      * The console command description.
@@ -19,20 +19,25 @@ class SwaggerCommand extends \Illuminate\Console\Command
     protected $description = 'Generate Swagger JSON file from your annotations';
 
     /**
-     * Create a new command instance.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        //
+        $root = app()->basePath();
+
+        $path = $this->option('path') ?? $root . '/swagger.json';
+
+        $swagger = \Swagger\scan($root, [
+            'exclude' => [
+                'tests',
+                'vendor',
+            ],
+        ]);
+
+        file_put_contents($path, $swagger);
+
+        $this->info('Generated at "' . realpath($path) . '"');
     }
 }
