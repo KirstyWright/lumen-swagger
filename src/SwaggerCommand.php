985f6d7fa1ed82f5d6734b0da60b16b82637887a
Benjamin Crozat <benjamin@croz.at>
2018-01-05 13:31:41 +0100

    Added --scan option + clean up

--- src/SwaggerCommand.php
+++ src/SwaggerCommand.php
@@ -6,38 +6,37 @@ class SwaggerCommand extends \Illuminate\Console\Command
 {
     /**
      * The name and signature of the console command.
-     *
-     * @var string
      */
-    protected $signature = 'swagger {--p|path=}';
+    protected $signature = 'swagger {--p|path=} {--s|scan=}';
 
     /**
      * The console command description.
-     *
-     * @var string
      */
     protected $description = 'Generate Swagger JSON file from your annotations';
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
     public function handle()
     {
-        $root = app()->basePath();
+        if ($scan = $this->option('scan')) {
+            $swagger = \Swagger\scan($scan);
+        } else {
+            $swagger = \Swagger\scan(app()->basePath(), [
+                'exclude' => [
+                    'tests',
+                    'vendor',
+                ],
+            ]);
+        }
 
-        $path = $this->option('path') ?? $root . '/swagger.json';
-
-        $swagger = \Swagger\scan($root, [
-            'exclude' => [
-                'tests',
-                'vendor',
-            ],
-        ]);
-
-        file_put_contents($path, $swagger);
+        file_put_contents($this->getPath(), $swagger);
 
         $this->info('Generated at "' . realpath($path) . '"');
+    }
+
+    protected function getPath() : string
+    {
+        return $this->option('path') ?? app()->basePath() . '/swagger.json';
     }
 }
