0c3447639709e30708c3a02e961146312912c5f2
Benjamin Crozat <benjamin.crozat@lagardere-active.com>
2018-05-17 09:51:55 +0200

    Removed composer.lock + clean up

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,6 @@ abstract class TestCase extends Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app)
     {
-        return [
-            BC\LumenSwagger\ServiceProvider::class,
-        ];
+        return [BC\LumenSwagger\ServiceProvider::class];
     }
 }
